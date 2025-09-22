94f5669f0596e6c1b4be40fbb011e29ef25e6c3d
JuanmaM-G <kporta715@gmail.com>
2025-09-22 18:58:42 -0500

    pruebas

--- app.py
+++ app.py
@@ -18,14 +18,64 @@ def footer():
 @app.route('/primer')
 def primer():
     return render_template ('primer.html')
+# -------------------------------------------------------------------------------
+                # ==== COLOMETRIA ====
+# -------------------------------------------------------------------------------
+@app.route('/primerC')
+def primerC():
+    return render_template ('primerC.html') 
+
+@app.route('/primerCC')
+def primerCC():
+    return render_template ('primerCC.html') 
+
+@app.route('/primerCCC')
+def primerCCC():
+    return render_template ('primerCCC.html') 
+
+# -------------------------------------------------------------------------------
 
 @app.route('/segundo')
 def segundo():
     return render_template ('segundo.html')
 
+# -------------------------------------------------------------------------------
+                # ==== COLOMETRIA ====
+# -------------------------------------------------------------------------------
+
+@app.route('/segundoC')
+def segundoC():
+    return render_template ('segundoC.html')
+
+@app.route('/segundoCC')
+def segundoCC():
+    return render_template ('segundoCC.html')
+
+@app.route('/segundoCCC')
+def segundoCCC():
+    return render_template ('segundoCCC.html')
+
+# -------------------------------------------------------------------------------
 @app.route('/tercero')
 def tercero():
     return render_template ('tercero.html')
+
+# -------------------------------------------------------------------------------
+                # ==== COLOMETRIA ====
+# -------------------------------------------------------------------------------
+@app.route('/terceroC')
+def terceroC(): 
+    return render_template ('terceroC.html')
+
+@app.route('/terceroCC')
+def terceroCC():
+    return render_template ('terceroCC.html')
+
+@app.route('/terceroCCC')
+def terceroCCC():
+    return render_template ('terceroCCC.html')
+
+
 # ------------------------------------------------------------------------------
 if __name__ == '__main__':
     app.run(debug=True)
