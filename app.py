7074c30fc0d816cd1e7c93bac5d34907959fc1b0
JuanmaM-G <kporta715@gmail.com>
2025-09-24 15:09:24 -0500

    fin colometria

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template
-from flask_session import Session
+
 
 app = Flask(__name__)
 
@@ -14,69 +14,6 @@ def navbar():
 @app.route('/footer')
 def footer():
     return render_template ('footer.html')
-
-@app.route('/primer')
-def primer():
-    return render_template ('primer.html')
-# -------------------------------------------------------------------------------
-                # ==== COLOMETRIA ====
-# -------------------------------------------------------------------------------
-@app.route('/primerC')
-def primerC():
-    return render_template ('primerC.html') 
-
-@app.route('/primerCC')
-def primerCC():
-    return render_template ('primerCC.html') 
-
-@app.route('/primerCCC')
-def primerCCC():
-    return render_template ('primerCCC.html') 
-
-# -------------------------------------------------------------------------------
-# si 
-@app.route('/segundo')
-def segundo():
-    return render_template ('segundo.html')
-
-# -------------------------------------------------------------------------------
-                # ==== COLOMETRIA ====
-# -------------------------------------------------------------------------------
-
-# si 
-@app.route('/segundoC')
-def segundoC():
-    return render_template ('segundoC.html')
-
-# si 
-@app.route('/segundoCC')
-def segundoCC():
-    return render_template ('segundoCC.html')
-
-@app.route('/segundoCCC')
-def segundoCCC():
-    return render_template ('segundoCCC.html')
-
-# -------------------------------------------------------------------------------
-@app.route('/tercero')
-def tercero():
-    return render_template ('tercero.html')
-
-# -------------------------------------------------------------------------------
-                # ==== COLOMETRIA ====
-# -------------------------------------------------------------------------------
-# si 
-@app.route('/terceroC')
-def terceroC(): 
-    return render_template ('terceroC.html')
-
-@app.route('/terceroCC')
-def terceroCC():
-    return render_template ('terceroCC.html')
-
-@app.route('/terceroCCC')
-def terceroCCC():
-    return render_template ('terceroCCC.html')
 
 
 # ------------------------------------------------------------------------------
